350e63a3872e6ee85bdd76f8c266b3530fbd68cf
oscarl11 <74116521+oscarl11@users.noreply.github.com>
2024-02-21 22:38:44 -0500

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,53 +1,381 @@
+!pip install --user -r requirements.txt
+
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-import os
-import pickle
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
-from sklearn.metrics import mean_squared_log_error, mean_absolute_error
+from IPython.display import display
+from pandas.api.types import CategoricalDtype
 
-def import_dataset(path):
-    df = pd.read_csv(path, parse_dates = ['saledate'])
+from category_encoders import MEstimateEncoder
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import mutual_info_regression
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.metrics import (r2_score,mean_squared_error,
+                             mean_squared_log_error,make_scorer)
+from xgboost import XGBRegressor
+
+def load_and_preprocess_data():
+    # Read data
+    path = "data/"
+    df_train = pd.read_csv(path + "train.csv", index_col="Id")
+    df_test = pd.read_csv(path + "test.csv", index_col="Id")
+    # Merge the splits so we can process them together
+    df = pd.concat([df_train, df_test])
+    # Preprocessing
+    df = clean(df)
+    df = encode(df)
+    df = impute(df)
+    # Reform splits
+    df_train = df.loc[df_train.index, :]
+    df_test = df.loc[df_test.index, :]
+    return df_train, df_test 
+
+df_train, df_test = load_and_preprocess_data()
+
+df.Exterior2nd.unique()
+
+#Cleaning the data
+def clean(df):
+    df["Exterior2nd"] = df["Exterior2nd"].replace({"Brk Cmn": "BrkComm"})
+    # Some values of GarageYrBlt are corrupt, so we'll replace them
+    # with the year the house was built
+    df["GarageYrBlt"] = df["GarageYrBlt"].where(df.GarageYrBlt <= 2010, df.YearBuilt)
+    # Names beginning with numbers are awkward to work with
+    df.rename(columns={
+        "1stFlrSF": "FirstFlrSF",
+        "2ndFlrSF": "SecondFlrSF",
+        "3SsnPorch": "Threeseasonporch",
+    }, inplace=True,
+    )
     return df
 
-df_test_and_valid = import_dataset("/data/TrainAndValid.csv") 
+# The numeric features are already encoded correctly (`float` for
+# continuous, `int` for discrete), but the categoricals we'll need to
+# do ourselves. Note in particular, that the `MSSubClass` feature is
+# read as an `int` type, but is actually a (nominative) categorical.
+
+# The nominative (unordered) categorical features
+features_nom = ["MSSubClass", "MSZoning", "Street", "Alley", "LandContour", "LotConfig", "Neighborhood", "Condition1", "Condition2", "BldgType", "HouseStyle", "RoofStyle", "RoofMatl", "Exterior1st", "Exterior2nd", "MasVnrType", "Foundation", "Heating", "CentralAir", "GarageType", "MiscFeature", "SaleType", "SaleCondition"]
+
+# Pandas calls the categories "levels"
+five_levels = ["Po", "Fa", "TA", "Gd", "Ex"]
+ten_levels = list(range(10))
+
+ordered_levels = {
+    "OverallQual": ten_levels,
+    "OverallCond": ten_levels,
+    "ExterQual": five_levels,
+    "ExterCond": five_levels,
+    "BsmtQual": five_levels,
+    "BsmtCond": five_levels,
+    "HeatingQC": five_levels,
+    "KitchenQual": five_levels,
+    "FireplaceQu": five_levels,
+    "GarageQual": five_levels,
+    "GarageCond": five_levels,
+    "PoolQC": five_levels,
+    "LotShape": ["Reg", "IR1", "IR2", "IR3"],
+    "LandSlope": ["Sev", "Mod", "Gtl"],
+    "BsmtExposure": ["No", "Mn", "Av", "Gd"],
+    "BsmtFinType1": ["Unf", "LwQ", "Rec", "BLQ", "ALQ", "GLQ"],
+    "BsmtFinType2": ["Unf", "LwQ", "Rec", "BLQ", "ALQ", "GLQ"],
+    "Functional": ["Sal", "Sev", "Maj1", "Maj2", "Mod", "Min2", "Min1", "Typ"],
+    "GarageFinish": ["Unf", "RFn", "Fin"],
+    "PavedDrive": ["N", "P", "Y"],
+    "Utilities": ["NoSeWa", "NoSewr", "AllPub"],
+    "CentralAir": ["N", "Y"],
+    "Electrical": ["Mix", "FuseP", "FuseF", "FuseA", "SBrkr"],
+    "Fence": ["MnWw", "GdWo", "MnPrv", "GdPrv"],
+}
+
+# Add a None level for missing values
+ordered_levels = {key: ["None"] + value for key, value in
+                  ordered_levels.items()}
 
 
-def enrich_df(df):
-    """
-    Adds following columns to dataframe saleYear, saleMonth, saleDay, saledayOfWeek, saleDayOfYear
-    """
-    temp_dict={
-    "saleYear":"year",
-    "saleMonth":"month",
-    "saleDay":"day",
-    "saleDayOfWeek":"dayofweek",
-    "saleDayOfYear":"dayofyear"
-    }
+def encode(df):
+    # Nominal categories
+    for name in features_nom:
+        df[name] = df[name].astype("category")
+        # Add a None category for missing values
+        if "None" not in df[name].cat.categories:
+            df[name].cat.add_categories("None", inplace=True)
+    # Ordinal categories
+    for name, levels in ordered_levels.items():
+        df[name] = df[name].astype(CategoricalDtype(levels,
+                                                    ordered=True))
+    return df
+
+def impute(df):
+    for name in df.select_dtypes("number"):
+        df[name] = df[name].fillna(0)
+    for name in df.select_dtypes("category"):
+        df[name] = df[name].fillna("None")
+    return df
+
+def make_mi_scores(X, y):
+    X = X.copy()
+    for colname in X.select_dtypes(["object", "category"]):
+        X[colname], _ = X[colname].factorize()
+    # All discrete features should now have integer dtypes
+    discrete_features = [pd.api.types.is_integer_dtype(t) for t in X.dtypes]
+    mi_scores = mutual_info_regression(X, y, discrete_features=discrete_features, random_state=0)
+    mi_scores = pd.Series(mi_scores, name="MI Scores", index=X.columns)
+    mi_scores = mi_scores.sort_values(ascending=False)
+    return mi_scores
+
+def plot_mi_scores(scores):
+    scores = scores.sort_values(ascending=True)
+    width = np.arange(len(scores))
+    ticks = list(scores.index)
+    plt.barh(width, scores)
+    plt.yticks(width, ticks)
+    plt.title("Mutual Information Scores")
+
+X = df_train.copy()
+y = X.pop("SalePrice")
+mi_scores = make_mi_scores(X, y)
+
+def drop_uninformative(df, mi_scores):
+    return df.loc[:, mi_scores > 0.0]
+
+def label_encode(df):
+    X = df.copy()
+    for colname in X.select_dtypes(["category"]):
+        X[colname] = X[colname].cat.codes
+    return X
+
+ef mathematical_transforms(df):
+    X = pd.DataFrame()  # dataframe to hold new features
+    X["LivLotRatio"] = df.GrLivArea / df.LotArea
+    X["Spaciousness"] = (df.FirstFlrSF + df.SecondFlrSF) / df.TotRmsAbvGrd
+    return X
+
+def interactions(df):
+    X = pd.get_dummies(df.BldgType, prefix="Bldg")
+    X = X.mul(df.GrLivArea, axis=0)
+    return X
+
+def counts(df):
+    X = pd.DataFrame()
+    X["PorchTypes"] = df[[
+        "WoodDeckSF",
+        "OpenPorchSF",
+        "EnclosedPorch",
+        "Threeseasonporch",
+        "ScreenPorch",
+    ]].gt(0.0).sum(axis=1)
+    return X
+
+def break_down(df):
+    X = pd.DataFrame()
+    X["MSClass"] = df.MSSubClass.str.split("_", n=1, expand=True)[0]
+    return X
+
+def group_transforms(df):
+    X = pd.DataFrame()
+    X["MedNhbdArea"] = df.groupby("Neighborhood")["GrLivArea"].transform("median")
+    return X
+
+cluster_features = [
+    "LotArea",
+    "TotalBsmtSF",
+    "FirstFlrSF",
+    "SecondFlrSF",
+    "GrLivArea",
+]
+
+
+def cluster_labels(df, features, n_clusters=20):
+    X = df.copy()
+    X_scaled = X.loc[:, features]
+    X_scaled = (X_scaled - X_scaled.mean(axis=0)) / X_scaled.std(axis=0)
+    kmeans = KMeans(n_clusters=n_clusters, n_init=50, random_state=0)
+    X_new = pd.DataFrame()
+    X_new["Cluster"] = kmeans.fit_predict(X_scaled)
+    return X_new
+
+def cluster_distance(df, features, n_clusters=20):
+    X = df.copy()
+    X_scaled = X.loc[:, features]
+    X_scaled = (X_scaled - X_scaled.mean(axis=0)) / X_scaled.std(axis=0)
+    kmeans = KMeans(n_clusters=20, n_init=50, random_state=0)
+    X_cd = kmeans.fit_transform(X_scaled)
+    # Label features and join to dataset
+    X_cd = pd.DataFrame(
+        X_cd, columns=[f"Centroid_{i}" for i in range(X_cd.shape[1])]
+    )
+    return X_cd
+
+def apply_pca(X, standardize=True):
+    # Standardize
+    if standardize:
+        X = (X - X.mean(axis=0)) / X.std(axis=0)
+    # Create principal components
+    pca = PCA()
+    X_pca = pca.fit_transform(X)
+    # Convert to dataframe
+    component_names = [f"PC{i+1}" for i in range(X_pca.shape[1])]
+    X_pca = pd.DataFrame(X_pca, columns=component_names)
+    # Create loadings
+    loadings = pd.DataFrame(
+        pca.components_.T,  # transpose the matrix of loadings
+        columns=component_names,  # so the columns are the principal components
+        index=X.columns,  # and the rows are the original features
+    )
+    return pca, X_pca, loadings
+
+def pca_inspired(df):
+    X = pd.DataFrame()
+    X["Feature1"] = df.GrLivArea + df.TotalBsmtSF
+    X["Feature2"] = df.YearRemodAdd * df.TotalBsmtSF
+    return X
+def pca_components(df, features):
+    X = df.loc[:, features]
+    _, X_pca, _ = apply_pca(X)
+    return X_pca
+
+
+pca_features = [
+    "GarageArea",
+    "YearRemodAdd",
+    "TotalBsmtSF",
+    "GrLivArea",
+]
+
+def plot_variance(pca, width=8, dpi=100):
+    # Create figure
+    fig, axs = plt.subplots(1, 2)
+    n = pca.n_components_
+    grid = np.arange(1, n + 1)
+    # Explained variance
+    evr = pca.explained_variance_ratio_
+    axs[0].bar(grid, evr)
+    axs[0].set(
+        xlabel="Component", title="% Explained Variance", ylim=(0.0, 1.0)
+    )
+    # Cumulative Variance
+    cv = np.cumsum(evr)
+    axs[1].plot(np.r_[0, grid], np.r_[0, cv], "o-")
+    axs[1].set(
+        xlabel="Component", title="% Cumulative Variance", ylim=(0.0, 1.0)
+    )
+    # Set up figure
+    fig.set(figwidth=8, dpi=100)
+    return axs
+
+import seaborn as sns
+def corrplot(df, method="pearson", annot=True, **kwargs):
+    sns.clustermap(
+        df.corr(method),
+        vmin=-1.0,
+        vmax=1.0,
+        cmap="icefire",
+        method="complete",
+        annot=annot,
+        **kwargs,
+    )
+
+corrplot(df_train, annot=None)
+
+class CrossFoldEncoder:
+    def __init__(self, encoder, **kwargs):
+        self.encoder_ = encoder
+        self.kwargs_ = kwargs  # keyword arguments for the encoder
+        self.cv_ = KFold(n_splits=5)
+
+    # Fit an encoder on one split and transform the feature on the
+    # other. Iterating over the splits in all folds gives a complete
+    # transformation. We also now have one trained encoder on each
+    # fold.
+    def fit_transform(self, X, y, cols):
+        self.fitted_encoders_ = []
+        self.cols_ = cols
+        X_encoded = []
+        for idx_encode, idx_train in self.cv_.split(X):
+            fitted_encoder = self.encoder_(cols=cols, **self.kwargs_)
+            fitted_encoder.fit(
+                X.iloc[idx_encode, :], y.iloc[idx_encode],
+            )
+            X_encoded.append(fitted_encoder.transform(X.iloc[idx_train, :])[cols])
+            self.fitted_encoders_.append(fitted_encoder)
+        X_encoded = pd.concat(X_encoded)
+        X_encoded.columns = [name + "_encoded" for name in X_encoded.columns]
+        return X_encoded
+
+ # To transform the test data, average the encodings learned from
+    # each fold.
+    def transform(self, X):
+        from functools import reduce
+
+        X_encoded_list = []
+        for fitted_encoder in self.fitted_encoders_:
+            X_encoded = fitted_encoder.transform(X)
+            X_encoded_list.append(X_encoded[self.cols_])
+        X_encoded = reduce(
+            lambda x, y: x.add(y, fill_value=0), X_encoded_list
+        ) / len(X_encoded_list)
+        X_encoded.columns = [name + "_encoded" for name in X_encoded.columns]
+        return X_encoded
+
+def create_features(df, df_test=None):
+    X = df.copy()
+    y = X.pop("SalePrice")
+    mi_scores = make_mi_scores(X, y)
+
+    # Combine splits if test data is given
+    #
+    # If we're creating features for test set predictions, we should
+    # use all the data we have available. After creating our features,
+    # we'll recreate the splits.
+    if df_test is not None:
+        X_test = df_test.copy()
+        X_test.pop("SalePrice")
+        X = pd.concat([X, X_test])
+
+    # Mutual Information
+    X = drop_uninformative(X, mi_scores)
     
-    for column, attribute in temp_dict.items():
-        df[column] = df["saledate"].dt.__getattribute__(attribute)
-    return df
+    X.info()
 
-def preprocess_dataframe_for_model(df):
-    # change all srting type to categorical type
-    for label, content in df.items():
-        if pd.api.types.is_string_dtype(content):
-            df[label]=df[label].astype("category").cat.as_ordered()
-            
-    # enrich the dataframe 
-    enrich_df(df)
-    df.drop("saledate",axis=1,inplace=True)
+ # Experimentation can be done with uncommented codes amd transformations and che
+
+    # Transformations
+    X = X.join(mathematical_transforms(X))
+    X = X.join(interactions(X))
+    X = X.join(counts(X))
+    # X = X.join(break_down(X))
+    X = X.join(group_transforms(X))
+
+    # Clustering
+    # X = X.join(cluster_labels(X, cluster_features, n_clusters=20))
+    # X = X.join(cluster_distance(X, cluster_features, n_clusters=20))
+
+    # PCA
+    X = X.join(pca_inspired(X))
+    # X = X.join(pca_components(X, pca_features))
+    # X = X.join(indicate_outliers(X))
+
+    X = label_encode(X)
     
-    # fill the numerical missing values with median and non-numerical values with their (category no. + 1)      
-    for label, content in df.items():
-        if pd.api.types.is_numeric_dtype(content):
-            if pd.isnull(content).sum():
-                df[label]=content.fillna(content.median())
-        else:
-            df[label]=pd.Categorical(content).codes+1
-    return df
+    X.info()
+    
+
+    # Reform splits
+    if df_test is not None:
+        X_test = X.loc[df_test.index, :]
+        X.drop(df_test.index, inplace=True)
+
+#     # Target Encoder
+#     encoder = CrossFoldEncoder(MEstimateEncoder, m=1)
+#     X = X.join(encoder.fit_transform(X, y, cols=["MSSubClass"]))
+#     if df_test is not None:
+#         X_test = X_test.join(encoder.transform(X_test))
+
+    if df_test is not None:
+        return X, X_test
+    else:
+        return X
 
 
-df_test_and_valid_modified=preprocess_dataframe_for_model(df_test_and_valid)
-
-df_test_and_valid_modified.to_pickle("/data/df_test_and_valid_modified.pkl") 
